cbd0301fe09cdcaeee158fd83466580d4f351051
kissshot1104 <kissshot.heartunderblade1104@gmail.com>
2023-12-28 15:18:12 +0900

    feat: restapi session로그인

--- src/main/java/com/mysite/medium/SecurityConfig.java
+++ src/main/java/com/mysite/medium/SecurityConfig.java
@@ -1,53 +1,85 @@
 package com.mysite.medium;
 
+import static org.springframework.security.web.util.matcher.AntPathRequestMatcher.antMatcher;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.header.writers.frameoptions.XFrameOptionsHeaderWriter;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.security.web.authentication.AbstractAuthenticationProcessingFilter;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity(prePostEnabled = true)
 public class SecurityConfig {
     @Bean
-    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests().requestMatchers(
-                new AntPathRequestMatcher("/**")).permitAll()
-        	.and()
-        		.csrf().ignoringRequestMatchers(
-        				new AntPathRequestMatcher("/h2-console/**"))
-        	.and()
-                .headers()
-                .addHeaderWriter(new XFrameOptionsHeaderWriter(
-                        XFrameOptionsHeaderWriter.XFrameOptionsMode.SAMEORIGIN))
-            .and()
-                .formLogin()
-                .loginPage("/user/login")
-                .defaultSuccessUrl("/")
-            .and()
-                .logout()
-                .logoutRequestMatcher(new AntPathRequestMatcher("/user/logout"))
-                .logoutSuccessUrl("/")
-                .invalidateHttpSession(true)
-        		;
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        // 추가된 코드
+        AuthenticationManagerBuilder sharedObject = http.getSharedObject(AuthenticationManagerBuilder.class);
+        AuthenticationManager authenticationManager = sharedObject.build();
+
+        http.authenticationManager(authenticationManager);
+
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .formLogin(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(authorizeRequest ->
+                        authorizeRequest
+                                .requestMatchers(
+                                        antMatcher("/auth/**")
+                                ).authenticated()
+                                .requestMatchers(
+                                        antMatcher("/h2-console/**")
+                                ).permitAll()
+                )
+                // 추가된 코드
+                .addFilterAt(
+                        this.abstractAuthenticationProcessingFilter(authenticationManager),
+                        UsernamePasswordAuthenticationFilter.class)
+                .headers(
+                        headersConfigurer ->
+                                headersConfigurer
+                                        .frameOptions(
+                                                HeadersConfigurer.FrameOptionsConfig::sameOrigin
+                                        )
+                                        .contentSecurityPolicy(policyConfig ->
+                                                policyConfig.policyDirectives(
+                                                        "script-src 'self'; " + "img-src 'self'; " +
+                                                                "font-src 'self' data:; " + "default-src 'self'; " +
+                                                                "frame-src 'self'"
+                                                )
+                                        )
+                );
+
         return http.build();
     }
-    
+
     @Bean
     PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
-    
+
     @Bean
-    AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+    AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration)
+            throws Exception {
         return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    public AbstractAuthenticationProcessingFilter abstractAuthenticationProcessingFilter(
+            final AuthenticationManager authenticationManager) {
+        return new LoginAuthenticationFilter(
+                "/api/login",
+                authenticationManager
+        );
     }
 }
