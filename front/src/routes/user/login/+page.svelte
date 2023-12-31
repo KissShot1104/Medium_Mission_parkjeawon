4c9bcd745e034d0520ec2ff47554df289cb7b3ef
kissshot1104 <kissshot.heartunderblade1104@gmail.com>
2024-01-01 00:03:33 +0900

    feat: 로그인 기능 추가

---
+++ front/src/routes/user/login/+page.svelte
@@ -0,0 +1,43 @@
+<script>
+	import axios from "axios";
+
+	let username = '';
+	let password = '';
+
+
+	function login() {
+		return new Promise(async (resolve, reject) => {
+			try {
+				let res = await axios.post(
+					`http://localhost:8080/api/login`,
+					{ username: username, password: password }
+				);
+				console.log(res);
+			} catch (error) {
+				reject(error);
+			} finally {
+				console.log('long done');
+			}
+		});
+	}
+</script>
+
+<div>
+	<input
+		type="text"
+		bind:value={username}
+		placeholder="아이디를 입력해주세요"
+		class="input input-bordered input-primary w-full max-w-xs"
+	/>
+</div>
+
+<div>
+	<input
+		type="text"
+		bind:value={password}
+		placeholder="비밀번호를 입력해주세요"
+		class="input input-bordered input-primary w-full max-w-xs"
+	/>
+</div>
+
+<button on:click={login}>로그인</button>
