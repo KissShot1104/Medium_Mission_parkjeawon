a24b15a306c320a213b35539fb4b3c438951dd4d
kissshot1104 <kissshot.heartunderblade1104@gmail.com>
2023-12-31 17:15:17 +0900

    Doc: 참고용 소스

---
+++ src/main/java/com/mysite/medium/user/controller/UserRestController.java
@@ -0,0 +1,21 @@
+//package com.mysite.medium.user.controller;
+//
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.web.bind.annotation.CrossOrigin;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RequestParam;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//@RestController
+//@CrossOrigin(origins = "http://localhost:5173")
+//@RequiredArgsConstructor
+//@RequestMapping("/user")
+//public class UserRestController {
+//
+//    @PostMapping("/login")
+//    public String login(@RequestParam) {
+//
+//    }
+//}
