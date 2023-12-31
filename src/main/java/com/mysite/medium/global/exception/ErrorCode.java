3b9f560a0bcb7a2fc9dcb2bf521e2af20d092fe1
kissshot1104 <kissshot.heartunderblade1104@gmail.com>
2023-12-31 14:16:34 +0900

    feat: 에러코드 추가

--- src/main/java/com/mysite/medium/global/exception/ErrorCode.java
+++ src/main/java/com/mysite/medium/global/exception/ErrorCode.java
@@ -9,8 +9,9 @@ public enum ErrorCode {
     INVALID_INPUT_VALUE(400, "C_002", "적절하지 않은 요청 값입니다."),
     NOT_FOUND(404, "C_003", "존재하지 않는 값을 요청했습니다."),
     FAIL_LOGIN(400, "AU_001", "아이디 또는 패스워드가 틀립니다."),
-    DUPLICATED_LOGIN_ID(400, "AU_002", "이미 존재하는 ID입니다."),
-    DUPLICATED_EMAIL(400, "AU_003", "이미 존재하는 E-mail입니다."),
+    UNAUTHORIZED_USER(400, "AU_002", "권한이 없는 사용자입니다."),
+    DUPLICATED_LOGIN_ID(400, "AU_003", "이미 존재하는 ID입니다."),
+    DUPLICATED_EMAIL(400, "AU_004", "이미 존재하는 E-mail입니다."),
     INTERNAL_SERVER_ERROR(500, "C_004", "서버에 문제가 생겼습니다.");
 
     private final String code;
