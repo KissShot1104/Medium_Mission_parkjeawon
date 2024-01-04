3d675d13303608a580cefa8f164e47c8ad1b8aad
kissshot1104 <kissshot.heartunderblade1104@gmail.com>
2024-01-04 13:46:41 +0900

    refactor: test코드 추가

--- src/main/java/com/mysite/medium/member/DbInit.java
+++ src/main/java/com/mysite/medium/member/DbInit.java
@@ -1,14 +1,11 @@
-package com.mysite.medium.user;
+package com.mysite.medium.member;
 
 import com.mysite.medium.article.entity.Article;
 import com.mysite.medium.article.repository.ArticleRepository;
-import com.mysite.medium.article.service.ArticleService;
 import com.mysite.medium.comment.entity.Comment;
 import com.mysite.medium.comment.repository.CommentRepository;
-import com.mysite.medium.comment.service.CommentService;
-import com.mysite.medium.user.entity.SiteUser;
-import com.mysite.medium.user.repository.UserRepository;
-import com.mysite.medium.user.service.UserService;
+import com.mysite.medium.member.entity.Member;
+import com.mysite.medium.member.repository.MemberRepository;
 import jakarta.annotation.PostConstruct;
 import java.util.ArrayList;
 import java.util.List;
@@ -20,13 +17,9 @@ import org.springframework.transaction.annotation.Transactional;
 @Component
 @RequiredArgsConstructor
 public class DbInit {
-    private final UserRepository userRepository;
+    private final MemberRepository memberRepository;
     private final ArticleRepository articleRepository;
     private final CommentRepository commentRepository;
-    //private final CommentRepository commentRepository;
-    private final UserService userService;
-    private final ArticleService articleService;
-    private final CommentService commentService;
     private final PasswordEncoder passwordEncoder;
 
     @PostConstruct
@@ -35,27 +28,27 @@ public class DbInit {
 
 
 
-        //Init SiteUser
-        List<SiteUser> siteUsers = new ArrayList<>();
-        for (int i = 0; i < 10; i++) {
-            SiteUser siteUser = SiteUser.builder()
+        //Init Member
+        List<Member> members = new ArrayList<>();
+        for (int i = 0; i < 8; i++) {
+            Member member = Member.builder()
                     .username(i + "")
                     .password(passwordEncoder.encode(i + ""))
                     .email(i + "@" + i)
                     .isPaid(i % 2 == 0)
                     .build();
-            siteUsers.add(siteUser);
-            userRepository.save(siteUser);
+            members.add(member);
+            memberRepository.save(member);
 
         }
 
         //Init Article
         List<Article> articles = new ArrayList<>();
-        for (int i = 0; i < 50; i++) {
+        for (int i = 0; i < 100; i++) {
             Article article = Article.builder()
                     .subject("제목:" + i + "" + i + i)
                     .content("내용:" + i + "" + i + i)
-                    .author(siteUsers.get(i % 10))
+                    .author(members.get(i % 8))
                     .viewCount((long) i)
                     .build();
 
@@ -68,9 +61,9 @@ public class DbInit {
         for (int i = 0; i < 500; i++) {
             Comment comment = Comment.builder()
                     .content(i + "" + i + i + i)
-//                    .author(userService siteUsers.get(i % 10))
-                    .author(siteUsers.get(i % 10))
-                    .article(articles.get(i % 50))
+//                    .author(userService members.get(i % 10))
+                    .author(members.get(i % 7))
+                    .article(articles.get(i % 100))
                     .build();
 
             comments.add(comment);
